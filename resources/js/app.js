667f1129fe97b3628c1d7f092063905877a58e87
AjAsif49 <54905485+AjAsif49@users.noreply.github.com>
2021-10-15 22:27:13 +0600

    up

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,18 +2,21 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue').default;
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 Vue.use(VueRouter)
 
 
+//Router Import
+import {routes} from './routes';
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
+const router = new VueRouter({
+    routes, // short for `routes: routes`
+    mode: 'history'
+  })
 
 
 const app = new Vue({
     el: '#app',
+    router
 });
