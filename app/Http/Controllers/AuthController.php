e0e6aaf00307403cc91e5222bf14edf31a912b02
AjAsif49 <54905485+AjAsif49@users.noreply.github.com>
2021-10-21 23:29:23 +0600

    up

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,7 +34,7 @@ class AuthController extends Controller
             return $this->respondWithToken($token);
         }
 
-        return response()->json(['error' => 'Unauthorized'], 401);
+        return response()->json(['error' => 'Email or Password Invalid'], 401);
     }
 
     /**
