08b7e4cfdbf61b83f5e53a9ccfb77c0a61469ca6
AjAsif49 <54905485+AjAsif49@users.noreply.github.com>
2021-11-17 00:23:17 +0600

    Category CRUD

--- resources/js/components/employee/create.vue
+++ resources/js/components/employee/create.vue
@@ -86,7 +86,7 @@
               </div>
 
               <div class="form-group">
-                <button type="submit" class="btn btn-primary btn-block">Submit</button>
+                <button type="submit" class="btn btn-primary btn-block">Add Employee</button>
               </div>
               <hr>
             </form>
