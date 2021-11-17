7dbb7e9b6a998a56fd21a21a684dad3a34fb8ee8
AjAsif49 <54905485+AjAsif49@users.noreply.github.com>
2021-11-18 00:19:55 +0600

    product CRUD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -353,7 +353,17 @@
         </div>
         <!---Container Fluid-->
       </div>
- 
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>copyright &copy; <script> document.write(new Date().getFullYear()); </script> - developed by
+              <b><a href="https://www.facebook.com/AsifJalal49/" target="_blank">AJ</a></b>
+            </span>
+          </div>
+        </div>
+      </footer>
+      <!-- Footer -->
     </div>
   </div>
   </div>
